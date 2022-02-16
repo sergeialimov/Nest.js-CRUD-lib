e085da1aabe2d90806a5b7550dd4656fe77b7ac0
sergeialimov <8912652@gmail.com>
2022-02-16 17:07:58 +0500

    fill service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,41 @@
+// CRUD version
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+
+import { Company } from './company.entity';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+export class UsersService extends TypeOrmCrudService<User> {
+  constructor(@InjectRepository(Company) repo) {
+    super(repo);
   }
 }
+
+// Default version
+// import { Injectable } from '@nestjs/common';
+// import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+// @Injectable()
+// export class UsersService {
+//   create(createUserDto: CreateUserDto) {
+//     return 'This action adds a new user';
+//   }
+
+//   findAll() {
+//     return `This action returns all users`;
+//   }
+
+//   findOne(id: number) {
+//     return `This action returns a #${id} user`;
+//   }
+
+//   update(id: number, updateUserDto: UpdateUserDto) {
+//     return `This action updates a #${id} user`;
+//   }
+
+//   remove(id: number) {
+//     return `This action removes a #${id} user`;
+//   }
+// }
